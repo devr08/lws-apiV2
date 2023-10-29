7309398b31ff0c0efcea3636219f348e32ca73f4
devr08 <148674860+devr08@users.noreply.github.com>
2023-10-29 23:33:55 +0000

    init strapi

---
+++ src/api/info-lowongan/controllers/info-lowongan.ts
@@ -0,0 +1,7 @@
+/**
+ * info-lowongan controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::info-lowongan.info-lowongan');
---
+++ src/api/info-lowongan/services/info-lowongan.ts
@@ -0,0 +1,7 @@
+/**
+ * info-lowongan service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::info-lowongan.info-lowongan');
